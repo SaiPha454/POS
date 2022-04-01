1fbf2daf5bca475d467accc3c2b8a153be251149
SaiPha454 <cencer880@gmail.com>
2022-04-01 19:55:25 +0630

    Add seller update item feature

--- api/src/daos/imple/sellerImple.js
+++ api/src/daos/imple/sellerImple.js
@@ -43,7 +43,53 @@ const deleteById =async (id)=>{
     
 }
 
+/**
+ * 
+ * find a product item by id
+ * 
+ * @author Sai Marn Pha
+ * @param {ObjectId} id - the item id 
+ * 
+ * @returns Object
+ */
+const findById= async (id)=>{
+
+    return await Product.findOne({'_id': mongoose.Types.ObjectId(id)});
+}
+
+/**
+ * Update a product item information by id
+ * 
+ * @author Sai Marn Pha
+ * @param {ObjectId} id - product item id
+ * @param {string} name - new product name which will be updated
+ * @param {number} price - new price of the specified item
+ * @param {ObjectId} seller - seller id
+ * @param {ObjectId} category - category id 
+ * @param {string} description - new description of the specified item
+ * 
+ * @returns Object
+ */
+const update = async (id, name, price, seller, category, description)=>{
+
+    let filter = {
+        '_id': mongoose.Types.ObjectId(id)
+    }
+    let update = {
+        name,
+        description,
+        price,
+        seller,
+        category,
+        description
+    }
+
+    return await Product.updateOne(filter, update);
+}
+
 module.exports={
     insert,
-    deleteById
+    deleteById,
+    update,
+    findById
 }
--- api/src/daos/seller_dao.js
+++ api/src/daos/seller_dao.js
@@ -23,7 +23,35 @@ const insert = async (name, price, seller, category, description)=> sellerImple.
  */
 const deleteById = async (id)=> sellerImple.deleteById(id);
 
+/**
+ * 
+ * find a product item by id
+ * @author Sai Marn Pha
+ * 
+ * @param {ObjectId} id - the item id 
+ * 
+ * @returns Object
+ */
+const findById = async (id)=> sellerImple.findById(id);
+
+/**
+ * Update a product item information by id
+ * 
+ * @author Sai Marn Pha
+ * @param {ObjectId} id - product item id
+ * @param {string} name - new product name which will be updated
+ * @param {number} price - new price of the specified item
+ * @param {ObjectId} seller - seller id
+ * @param {ObjectId} category - category id 
+ * @param {string} description - new description of the specified item
+ * 
+ * @returns Object
+ */
+const update = async (id, name, price, seller, category, description)=> sellerImple.update(id, name, price, seller, category, description);
+
 module.exports={
     insert,
-    deleteById
+    deleteById,
+    findById,
+    update
 }
