2e3b0688eb14b7fade1082397d69ec20f489e5f8
SaiPha454 <cencer880@gmail.com>
2022-04-02 15:05:42 +0630

    Add get_item_by_id feature to common Endpoints

--- api/src/startUp/routes.js
+++ api/src/startUp/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const seller = require('../routes/seller');
+const common = require('../routes/common')
 
 module.exports = (app)=>{
 
     app.use(express.json())
     
     app.use('/seller', seller);
-
+    app.use('/', common);
 }
