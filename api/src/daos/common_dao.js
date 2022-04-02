dd4ef7a73b8de2a84f958aedc6a1d077ce3e3d75
SaiPha454 <cencer880@gmail.com>
2022-04-02 21:11:00 +0630

    Add get_all_items feature to common Endpoints

--- api/src/daos/common_dao.js
+++ api/src/daos/common_dao.js
@@ -10,6 +10,20 @@ const commonImple = require('./imple/commonImple')
  */
 const getItemById = async (id) => commonImple.getItemById(id);
 
+/**
+ * Get all products when no options are applied or filter, sort and paginate items with the specified options.
+ * 
+ * @author Sai Marn Pha
+ * @param {number} page - the pagination page index
+ * @param {number} limit - the number of items per page
+ * @param {Object} filter - filter object to parse in mongoDB query (eg. filter[name]= 'samsaung')
+ * @param {Object} sort - sort object to parse in mongoDB qeury (eg. sort[name]= -1)
+ * 
+ * @returns Object
+ */
+const getAllItems = async (page, limit, filter, sort)=> commonImple.getAllItems(page, limit, filter, sort);
+
 module.exports={
-    getItemById
+    getItemById,
+    getAllItems
 }
