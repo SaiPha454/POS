a28e23ff4f9f9d8f81d8d104bafc968859b8f5d0
SaiPha454 <cencer880@gmail.com>
2022-04-02 21:11:00 +0630

    Add get_all_items feature to common Endpoints

--- api/src/routes/common.js
+++ api/src/routes/common.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const commonController = require('../controllers/commonController');
 
 router.get('/products/:id', commonController.getItemById)
+router.get('/products', commonController.getAllItems);
 
 module.exports = router;
